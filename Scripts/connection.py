90f13a81a5f3ace5924949de540c377945d79540
Bruce Raup <bruceraup@gmail.com>
2024-05-13 15:44:47 -0600

    Framework for data transfer script

---
+++ Scripts/connection.py
@@ -0,0 +1,5 @@
+CONN = ('host=db.production.glims.apps.int.nsidc.org'
+        ' dbname=glims user=glims_ro password=hyouga')
+
+CONN_V2 = ('host=db.development.glims_v2.apps.int.nsidc.org'
+        ' dbname=glims user=glims_ro password=hyouga')
