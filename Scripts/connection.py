e3533143a21128058d12e0984054c51acd79a30f
Bruce Raup <bruceraup@gmail.com>
2024-05-14 12:08:54 -0600

    Mv pw to env var

--- Scripts/connection.py
+++ Scripts/connection.py
@@ -1,5 +1,9 @@
+import os
+
+pw = os.environ['GLIMS_DB_PW']
+
 CONN = ('host=db.production.glims.apps.int.nsidc.org'
-        ' dbname=glims user=glims_ro password=hyouga')
+        ' dbname=glims user=glims_ro password={}'.format(pw))
 
 CONN_V2 = ('host=db.development.glims_v2.apps.int.nsidc.org'
-        ' dbname=glims user=glims_ro password=hyouga')
+        ' dbname=glims user=glims_ro password={}'.format(pw))
