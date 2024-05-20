44bccf7cf68af40abdd59cd7a8d49e40934ff084
Bruce Raup <bruceraup@gmail.com>
2024-05-20 14:42:17 -0600

    Add default for value of GLIMS_DB_PW envar

--- Scripts/connection.py
+++ Scripts/connection.py
@@ -1,6 +1,6 @@
 import os
 
-pw = os.environ['GLIMS_DB_PW']
+pw = os.environ.get('GLIMS_DB_PW', None)
 
 CONN = ('host=db.production.glims.apps.int.nsidc.org'
         ' dbname=glims user=glims_ro password={}'.format(pw))
