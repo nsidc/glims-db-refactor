1b8d598f92e851b5a2191346a991b597f45a8951
Bruce Raup <bruceraup@gmail.com>
2024-05-28 16:10:45 -0600

    Fix up output SQL in various ways; add --debug flag

--- Scripts/mv_glims_to_new_db.py
+++ Scripts/mv_glims_to_new_db.py
@@ -8,7 +8,7 @@ and write to the new database.
 
 import os
 import sys
-import datetime as dt
+import datetime
 import argparse
 from collections import OrderedDict
 from collections import defaultdict
@@ -35,6 +35,7 @@ def setup_argument_parser():
     p = argparse.ArgumentParser()
 
     p.add_argument('-b', '--bbox', default='all',  help=b_help)
+    p.add_argument('-d', '--debug', action='store_true',  help='Run in debugging mode')
     p.add_argument('-q', '--quiet',   action='store_true', default=False, help="Quiet mode.  Don't print status messages")
 
     return(p)
@@ -91,7 +92,9 @@ def get_tables_list(debug=False):
     '''
 
     if debug:
-        rtndict = {'country': ()}  # for case with a geometry column
+        #rtndict = {'image': ()}  # to fix the datetime.datetime issue
+        rtndict = {'glacier_polygons': ()}  # for meat of the data transform
+        #rtndict = {'country': ()}  # for case with a geometry column
         #rtndict = {'reference_document': ()}  # for simple scalar case
         return rtndict
 
@@ -151,27 +154,6 @@ def check_table_dependencies(tables):
     return True
 
 
-#def extract_poly_only(poly_ewkt):
-#    '''
-#    extract_poly_only -- extract a Shapely geom object from a row of data
-#
-#    Input:  EWKT form of polygon (with SRID) (geom part of "row" result of DB query)
-#    Output: shapely object form (without SRID representation)
-#    '''
-#    srid_part, poly_part = poly_ewkt.split(';')
-#    return sloads(poly_part)
-#
-#
-#def add_srid_to_poly(poly_text):
-#    '''
-#    add_srid_to_poly -- add the SRID=4326 part to the EWKT representation of the polygon
-#
-#    Input:  shapely to_wkt() form of poly: 'POLYGON((4.8 4, 4.8 4.8, 5.2 4.8, 5.2 4, 4.8 4))'
-#    Output: EWKT form:           'SRID=4326;POLYGON((4.8 4, 4.8 4.8, 5.2 4.8, 5.2 4, 4.8 4))'
-#    '''
-#    srid='SRID=4326'
-#    return ';'.join([srid, poly_text])
-
 
 def connect_to_db():
     try:
@@ -209,25 +191,31 @@ def process_glacier_entities(T, dbh_old, dbh_new, args):
     # Select all entities from database (or those within bounding box)
 
     base_query = f'SELECT gd.glacier_id, gd.analysis_id, {T}.line_type, ST_AsEWKT({T}.glacier_polys) ' \
-                + 'FROM {T}, glacier_dynamic gd WHERE {T}.analysis_id=gd.analysis_id'
+                + f'FROM {T}, glacier_dynamic gd WHERE {T}.analysis_id=gd.analysis_id'
 
     if args.bbox == 'all':
         sql = base_query
     else:
         W, E, S, N = args.bbox.split(',')
-        # SRID?
-        region = f"ST_MakePolygon(ST_GeomFromText('LINESTRING({W} {S}, {E} {S}, {E} {N}, {W} {N}, {W} {S})'))"
-        sql = base_query + f' AND ST_Overlaps(region, {T}.glacier_polys)'
+        region = f"ST_GeomFromEWKT('SRID=4326;POLYGON(({W} {S}, {E} {S}, {E} {N}, {W} {N}, {W} {S}))')"
+        sql = base_query + f' AND {region} && {T}.glacier_polys'
 
     dbh_old.execute(sql)
     query_results = dbh_old.fetchall()      # list of tuples
 
-    move_sql = old_to_new_data_model(query_results)
+    if not args.quiet:
+        print(f"Selected {len(query_results)} records from table {T}", file=sys.stderr)
+
+    move_sql = old_to_new_data_model(query_results, args)
+
+    if not args.quiet:
+        print(f"Printing {len(move_sql)} SQL statements...", file=sys.stderr)
+
     for m in move_sql:
         print(m)
 
 
-def old_to_new_data_model(query_results):
+def old_to_new_data_model(query_results, args):
     ''' old_to_new_data_model - translate all query results from old to new data model
 
     Input:  query_results, a list of tuples
@@ -272,6 +260,7 @@ def old_to_new_data_model(query_results):
     rocks_by_glac_id = defaultdict(list)
     orphan_rocks_by_glac_id = defaultdict(list)
     new_glac_rec_by_glac_id = defaultdict(list)
+    move_sql = []  # List of SQL statements to insert into the new DB
 
     # Copy non-glacier-bounds entities directly, or collect
     # glacier-bounds/intrnl_rock entities for further processing.
@@ -279,18 +268,26 @@ def old_to_new_data_model(query_results):
     for row in query_results:
         gl_obj = Glacier_entity(row)
         if gl_obj.line_type in ('pro_lake', 'supra_lake', 'basin_bound', 'debris_cov'):
-            print(insert_row_as_simple_copy(T, row))
+            move_sql.append(insert_row_as_simple_copy(T, row))
         elif gl_obj.line_type == 'glac_bound':
-            bounds_by_glac_id[gid].append(gl_obj)
+            bounds_by_glac_id[gl_obj.gid].append(gl_obj)
         elif gl_obj.line_type == 'intrnl_rock':
-            rocks_by_glac_id[gid].append(gl_obj)
+            rocks_by_glac_id[gl_obj.gid].append(gl_obj)
         else:
             print("Warning: found unknown line_type: ", line_type, file=sys.stderr)
 
     # Now assemble glac_bound polys with holes
+    if not args.quiet:
+        print('Looping through glac_bound objects', file=sys.stderr)
+
     for gid, gl_obj_list in bounds_by_glac_id.items():
+
+        if not args.quiet:
+            print(f'{gid}:  Got {len(gl_obj_list)} piece(s)', file=sys.stderr)
+
         if len(gl_obj_list) > 1:
             # multiple glac_bound polys for this glacier
+            print(f"Warning: Just found multiple glac_bound outlines for {gid}", file=sys.stderr)
             pass
         else:
             # Single glac_bound polygon. Find any intrnl_rock polys
@@ -309,15 +306,25 @@ def old_to_new_data_model(query_results):
                 holey_geom = Polygon(bound_obj.sgeom.exterior, [e.sgeom.exterior for e in int_rocks])
                 bound_obj.sgeom = holey_geom
 
-    for o in gl_obj_list:
-        sql = f'INSERT INTO glacier_entities (analysis_id, line_type, entity_geom) VALUES ({o.aid}, {o.line_type}, {o.as_ewkt_with_srid()});'
-        print(sql)
+            # Prepare SQL
+            # geom_part will look something like this:
+
+            # ST_GeomFromEWKT('SRID=4326;POLYGON((-18.225858 79.933083 0,
+            #     -18.225269 79.932970 0, -18.225888 79.932975 0, -18.225858 79.933083 0))'));
+
+            geom_part = f"ST_GeomFromEWKT('{bound_obj.as_ewkt_with_srid()}')"
+            sql = f'INSERT INTO glacier_entities (analysis_id, line_type, entity_geom) VALUES ' \
+                  + f'({bound_obj.aid}, {bound_obj.line_type}, {geom_part});'
+            move_sql.append(sql)
+
+    return move_sql
 
 
 def insert_row_as_simple_copy(T, row):
     ''' insert_row_as_simple_copy -- print or do INSERT of unchanged row to unchanged table
     '''
     row_fixed = tuple(['NULL' if e is None else e for e in row])
+    row_fixed = tuple([e.isoformat() if type(e) is datetime.datetime else e for e in row_fixed])
     sql_out = f'INSERT INTO {T} VALUES {row_fixed};'
     sql_out = sql_out.replace("'NULL'", 'NULL')
     return sql_out
@@ -328,7 +335,7 @@ def do_db_move(args):
     Do major steps to move the database.
     """
 
-    tables = get_tables_list(debug=True)
+    tables = get_tables_list(debug=args.debug)
 
     if tables is not None:
         print("Got table list:", file=sys.stderr)
@@ -343,11 +350,17 @@ def do_db_move(args):
     for T in tables.keys():
         # Default is a simple copy to the new db
         if T in ('glacier_polygons'):
+            if not args.quiet:
+                print("Processing 'glacier_polygons' table ...", file=sys.stderr)
             process_glacier_entities(T, dbh_old, dbh_new, args)
         else:
             # Simple copy
             # Can pg_dump/pg_restore be part of this? Nah...
             # Select *, then do a COPY command?
+
+            if not args.quiet:
+                print(f"Processing '{T}' table ...", file=sys.stderr)
+
             sql = f'SELECT * from {T};'
             dbh_old.execute(sql)
             for row in dbh_old.fetchall():
@@ -375,7 +388,7 @@ def main():
     # Parse command-line.
     args = p.parse_args()
 
-    today_string = dt.date.today().isoformat()
+    today_string = datetime.date.today().isoformat()
 
     # Get environment
     user = os.environ['USER']
