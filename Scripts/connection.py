e65f0ff040c8e36db54e71364e10c79766e3cf75
Bruce Raup <bruceraup@gmail.com>
2024-05-29 15:46:43 -0600

    Correct connection info for new dev db

--- Scripts/connection.py
+++ Scripts/connection.py
@@ -5,5 +5,5 @@ pw = os.environ.get('GLIMS_DB_PW', None)
 CONN = ('host=db.production.glims.apps.int.nsidc.org'
         ' dbname=glims user=glims_ro password={}'.format(pw))
 
-CONN_V2 = ('host=db.development.glims_v2.apps.int.nsidc.org'
-        ' dbname=glims user=glims_ro password={}'.format(pw))
+CONN_V2 = ('host=db.development.glims.apps.int.nsidc.org'
+        ' dbname=glims_v2 user=glims_ro password={}'.format(pw))
