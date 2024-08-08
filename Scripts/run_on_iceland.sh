33d4e954acb30a2ed65b976027eb0957026de508
Bruce Raup <bruceraup@gmail.com>
2024-08-08 15:09:29 -0600

    Move util script out of repo

--- Scripts/run_on_iceland.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#GLIMS_DB_RO=hyouga GLIMS_DB_RW=kakeru ./mv_glims_to_new_db.py -b ' -25,63,-13,67' --transaction --write_to_db > iceland.sql
-GLIMS_DB_RO=hyouga GLIMS_DB_RW=kakeru ./mv_glims_to_new_db.py -b ' -25,63,-13,67' --write_to_db > iceland.sql
